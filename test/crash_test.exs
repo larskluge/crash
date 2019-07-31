ce0e08bf521be82ec241100984b165fe576f9ae5
Lars Kluge <l@larskluge.com>
2019-07-31 14:45:47 +0200

    Test to trigger segmentation fault

--- test/crash_test.exs
+++ test/crash_test.exs
@@ -3,6 +3,11 @@ defmodule CrashTest do
   doctest Crash
 
   test "greets the world" do
-    assert Crash.hello() == :world
+    flow_a = Flow.from_enumerable(1_000..1_100)
+    flow_b = Flow.from_enumerable(  500..1_100)
+
+    flow = Flow.window_join(:inner, flow_a, flow_b, Flow.Window.global, & &1, & &1, fn a, a -> a end)
+
+    assert flow |> Enum.sort |> Enum.take(3) == [1_000, 1_001, 1_002]
   end
 end
